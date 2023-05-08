66a55cb6256b8212285e8e16d2c957fbd253eca1
Lsapee <dlworhks94@gmail.com>
2023-05-08 11:49:10 +0900

    LeetCode 30days Apply Transform Over Each Element in Array

---
+++ 4day.js
@@ -0,0 +1,16 @@
+var map = function(arr, fn) {
+    const ans =[];
+    for(let i=0; i<arr.length; i++){
+        ans.push(fn(arr[i],i));
+    }
+    return ans;
+};
+
+// TS
+// function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+//     const ans:number[] =[];
+//     for(let i=0; i<arr.length; i++){
+//         ans.push(fn(arr[i],i));
+//     }
+//     return ans;
+// };
